764de7f89f87b9210ce343ddf3c25ec7543c2b8e
Ayoub Moustahfid <moustahfidayoub12@gmail.com>
2021-03-07 23:52:29 +0100

    new modification in partie rejoindre

--- src/core/Home.js
+++ src/core/Home.js
@@ -5,65 +5,67 @@ import {isAuthenticated} from "../auth/helpers"
 import {API_URL} from "./../config"
 import toastr from 'toastr';
 import "toastr/build/toastr.css";
+import axios from "axios"
 
 function Home(props) {
     
     const {participant, token} = isAuthenticated()
 
     const createGroup = () => {
-        fetch(`${API_URL}/groupMember/create/${participant._id}`, {
-            method: "POST",
-            headers: {
-                "Accept": "application/json",
-                "Content-Type": "application/json",
-                "Authorization": `Bearer ${token}`
-            },
-            body : JSON.stringify({participant})._id
-        }).then(res => res.json())
-          .then(res => {
-              if(res.error){
-                toastr.warning(res.error, 'Please Check Click !', {
+       axios.post(`${API_URL}/groupMember/create/${participant._id}`, {participant: participant._id}, {
+           headers: {
+               "Authorization": `Bearer ${token}`
+           }
+       }).then(res => {
+           if(res.error){
+            toastr.warning(res.error, 'Please Check Click !', {
                    positionClass: "toastr-bottom-left"
-                })
-              }else{
-                toastr.success('Group Member is Created', 'Welcome in Your Group Member', {
+                })  
+           }else{
+            toastr.success('Group Member is Created', 'Welcome in Your Group Member', {
                    positionClass: "toastr-bottom-left"
                 })
 
-                localStorage.setItem('group_member', JSON.stringify(res))
+                localStorage.setItem('group_member', JSON.stringify(res.data))
+
                 props.history.push('/group_member')
-              }
-          })
+           }
+       })
     }
 
     // =================== Start Code Creation Method Rejoindre in Group Member ============================
     const rejoindre = () => {
-         const codeStorage = localStorage.getItem('group_member')
-         const code = JSON.parse(codeStorage).groupMember.code;
-         
-         fetch(`${API_URL}/groupMember/rejoindre/${code}`,{
-            method: "PUT",
-            headers: {
-                "Accept": "application/json",
-                "Content-Type": "application/json",
-                "Authorization": `Bearer ${token}`
-            },
-            body : JSON.stringify({participant})._id
-         }).then(res => res.json())
-           .then(res => {
-               if (res.error){
-                toastr.warning(res.error, 'Please Check Click !', {
+         var codeStorage = localStorage.getItem('group_member')
+         const code = JSON.parse(codeStorage).data.groupMember.code;
+
+
+         axios.put(`${API_URL}/groupMember/rejoindre/${code}`, {participant: participant._id}, {
+           headers: {
+               "Authorization": `Bearer ${token}`
+           }
+       }).then(res => {
+           if(res.error){
+            toastr.warning(res.error, 'Please Check Click !', {
                    positionClass: "toastr-bottom-left"
-                })
-               }else{
-                toastr.success('Group Member is Created', 'Welcome in My Group Member', {
+                })  
+           }else{
+            toastr.success('Group Member is Created', 'Welcome in Your Group Member', {
                    positionClass: "toastr-bottom-left"
                 })
 
-                localStorage.setItem('rejoindre', JSON.stringify(res))
-                props.history.push('/group_member')
-               }
-           })
+             
+                const participant = JSON.parse(codeStorage).data.groupMember.participant
+                
+                if(participant.length > 3){
+                    props.history.push('/group_member')
+                }else{
+                    return toastr.error('If you check are 4 participant in Group', 'Has not 4 participant', {
+                        positionClass: "toastr-bottom-left"
+                    })
+                }
+           }
+       })
+        
     }
 
     return (
